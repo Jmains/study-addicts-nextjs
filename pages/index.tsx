d347a0e98088495e301f71b761c4ccbce66f3bc0
Jackson <jacksonmain9@gmail.com>
2020-12-16 13:27:20 -0800

    tailwindcss config

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Intreecate</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className="text-blue-400">Hello</h1>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    "./pages/**/*.js",
+    "./components/**/*.js",
+    "./pages/**/*.tsx",
+    "./components/**/*.tsx",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
